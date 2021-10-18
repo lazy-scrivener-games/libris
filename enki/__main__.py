ebd501d01c6818e2a587348d959b75cd8066dd1d
caesuric <illusorycaesura@gmail.com>
2021-10-18 16:31:40 -0400

    updated __main__.py with --no-validation option

--- enki/__main__.py
+++ enki/__main__.py
@@ -15,7 +15,7 @@ from .lib.data_extractors import get_json_data
 from .lib.pdf_builder import build_pdf
 from .lib.watch import watch
 
-def main(config_file_path: str, should_watch: bool, be_verbose: bool):
+def main(config_file_path: str, should_watch: bool, be_verbose: bool, skip_validation: bool):
     """
     Builds a PDF from a JSON configuration file that points to various Markdown source files.
     Optionally watches for changes.
@@ -24,9 +24,10 @@ def main(config_file_path: str, should_watch: bool, be_verbose: bool):
         config_file (str): The configuration file to be used.
         should_watch (bool): Whether to watch the source files for changes and re-compile.
         be_verbose (bool): Whether to print debugging information.
+        skip_validation (bool): Whether to skip JSON validation.
     """
     try:
-        config = get_config_and_validate(config_file_path)
+        config = get_config_and_validate(config_file_path, skip_validation)
     except jsonschema.exceptions.ValidationError as err:
         terminate_with_validation_error(err)
     if should_watch:
@@ -35,12 +36,13 @@ def main(config_file_path: str, should_watch: bool, be_verbose: bool):
     else:
         build_pdf(config, be_verbose)
 
-def get_config_and_validate(config_file_path: str) -> dict:
+def get_config_and_validate(config_file_path: str, skip_validation: bool) -> dict:
     """
     Retrieves the configuration file and validates against the schema.
 
     Args:
         config_file_path (str): Path to the configuration file.
+        skip_validation (bool): Whether to skip JSON validation.
 
     Returns:
         dict: The validated configuration object.
@@ -48,7 +50,8 @@ def get_config_and_validate(config_file_path: str) -> dict:
     config = get_json_data(config_file_path)
     dir_path = os.path.dirname(os.path.realpath(__file__))
     schema = get_json_data(os.path.join(dir_path, JSON_SCHEMA_PATH))
-    jsonschema.validate(config, schema)
+    if not skip_validation:
+        jsonschema.validate(config, schema)
     return config
 
 def terminate_with_validation_error(err: jsonschema.exceptions.ValidationError):
@@ -91,8 +94,14 @@ def handle_args() -> str:
         action='store_true',
         help='Prints additional logging data, including intermediate HTML.'
     )
+    parser.add_argument(
+        '-n',
+        '--no-validation',
+        action='store_true',
+        help='Skips JSON validation up-front. Use if you are getting URL resolution errors.'
+    )
     return parser.parse_args()
 
 if __name__ == '__main__':
     arguments = handle_args()
-    main(arguments.config_file, arguments.watch, arguments.verbose)
+    main(arguments.config_file, arguments.watch, arguments.verbose, arguments.no_validation)
