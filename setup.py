89fa6c4881debdfdb5258a13edcd4c49c27a1e90
caesuric <illusorycaesura@gmail.com>
2021-10-18 13:57:35 -0400

    updated requirements to include idna, avoiding crash on json schema parse

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     ],
     install_requires=[
         'beautifulsoup4 == 4.10.0',
+        'idna == 2.6',
         'Jinja2 == 3.0.2',
         'jsonschema == 4.0.0',
         'markdown2 == 2.4.1',
